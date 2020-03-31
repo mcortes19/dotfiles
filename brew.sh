33c86c0689c6afc6605bc1cfd5062ca795d4132d
Marvin J. Cortés <marvin.cortes@gmail.com>
2020-03-30 21:16:42 -0600

    Update brew install command

--- brew.sh
+++ brew.sh
@@ -5,7 +5,7 @@ if test ! $(which brew)
 then
   # Install the correct homebrew for each OS type
   echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 fi
 
