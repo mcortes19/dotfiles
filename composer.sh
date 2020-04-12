45ef41e2464b0bccb57269069029897fec80c141
Marvin J. Cortés <marvin.cortes@gmail.com>
2020-04-12 17:19:19 -0600

    Change drush version to 10.2

--- composer.sh
+++ composer.sh
@@ -1,6 +1,6 @@
 
 composer global require pantheon-systems/terminus:"^2.3"
-composer global require drush/drush:"^9.7"
+composer global require drush/drush:"^10.2"
 
 ln -sf "$HOME/.composer/vendor/pantheon-systems/terminus/bin/terminus" "/usr/local/bin"
 ln -sf "$HOME/.composer/vendor/bin/drush" "/usr/local/bin"
