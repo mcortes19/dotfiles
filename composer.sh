fe6a208115f595e25eb15fdf8850769f0f3ae8ee
Marvin J. Cortés <marvin.cortes@gmail.com>
2020-04-25 14:10:06 -0600

    Add PHPCS packages

--- composer.sh
+++ composer.sh
@@ -1,7 +1,7 @@
 
 composer global require pantheon-systems/terminus:"^2.3"
 composer global require drush/drush:"^10.2"
+composer global require drupal/coder: "^8.3"
+composer global require squizlabs/php_codesniffer: "^3.5"
 
-ln -sf "$HOME/.composer/vendor/pantheon-systems/terminus/bin/terminus" "/usr/local/bin"
-ln -sf "$HOME/.composer/vendor/bin/drush" "/usr/local/bin"
 echo "Composer packages installed."
